f8a9755adf823ae98705c917ca51a0b626ddfa97
zazaki110 <miyamotoyuichi8326@gmail.com>
2022-08-14 22:41:11 +0900

    kousin

--- resources/views/layouts/input_screen.blade.php
+++ resources/views/layouts/input_screen.blade.php
@@ -7,18 +7,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0,minimum-scale=1.0, maximum-scale=1.0,user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0,minimum-scale=1.0, maximum-scale=1.0,user-scalable=no"> -->
     <title>@yield('title')</title>
 </head>
 
 {{ csrf_field() }}
-<header class="header_main"><div class="header_menu"><a class="tukibetu" href="{{url('/tarukame_totalling',)}}" >月別データ</a>
-    <div class="logo"> <a href="{{url('/tarukame_home',)}}"><img class="logo_pic" src="{{ asset('img/mark_yen_okaikei.png') }}" alt="ホームロゴ"></a></div>
-    <a class="nyuryoku" href="{{url('/register_member',)}}" >入力画面</a><a class="nenbetu" href="{{url('/tarukame_nenbetu',)}}">年別データ</a><div class="user_name_touroku">こんにちは&nbsp&nbsp{{$UserName}}さん</div>
-    <div class="home_name">家計簿タルカメ</div>
+<header class="header_main"><div class="header_menu"><a class="tukibetu tukibetu_register" href="{{url('/tarukame_totalling',)}}" >月別データ</a>
+    <div class="logo logo_register"> <a href="{{url('/tarukame_home',)}}"><img class="logo_pic logo_pic_register" src="{{ asset('img/mark_yen_okaikei.png') }}" alt="ホームロゴ"></a></div>
+    <a class="nyuryoku nyuryoku_register" href="{{url('/register_member',)}}" >入力画面</a><a class="nenbetu nenbetu_register" href="{{url('/tarukame_nenbetu',)}}">年別データ</a><div class="user_name_touroku user_name_touroku_register">こんにちは&nbsp&nbsp{{$UserName}}さん</div>
+    <div class="home_name home_name_register">家計簿タルカメ</div>
     <form action="{{ route('logout') }}" method="post">
   @csrf
-  <input type="submit" class="logout_bu" value="ログアウト">
+  <input type="submit" class="logout_bu logout_bu_register" value="ログアウト">
 </form>
     </header>
 <body class="input_screen_backgrand">
