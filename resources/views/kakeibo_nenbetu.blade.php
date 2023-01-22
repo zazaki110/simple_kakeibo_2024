2c0c5b4d1584496572eabe107b39ee0201185860
zazaki110 <miyamotoyuichi8326@gmail.com>
2023-01-22 20:32:34 +0900

    更新

--- resources/views/kakeibo_nenbetu.blade.php
+++ resources/views/kakeibo_nenbetu.blade.php
@@ -14,7 +14,7 @@
     <a class="nyuryoku" href="{{url('/register_member',)}}">入力画面</a><a class="nenbetu" href="{{url('/tarukame_nenbetu',)}}">年別データ</a>
     <div class="user_name">こんにちは&nbsp&nbsp{{$UserName}}さん</div>
   </div>
-  <div class="home_name">家計簿タルカメ</div>
+  <div class="home_name">シンプル家計簿</div>
 
   <form action="{{ route('logout') }}" method="post">
     @csrf
